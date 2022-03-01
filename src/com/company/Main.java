9ed64205e783d5193bae17f7fd7950aeb81ba00e
jorgerodriguezhernandez <jorgexarach298@gmail.com>
2022-03-01 12:59:02 +0000

    Feature: introduccion de datos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,27 @@
 package com.company;
 
+import com.jcraft.jsch.*;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Session session = null;
+        ChannelExec channel = null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce usuario");
+        String usuario= sc.nextLine();
+        System.out.println("Introduce Contraseña");
+        String password= sc.nextLine();
+        System.out.println("Introduce puerto");
+        int puerto = sc.nextInt();
+        System.out.println("introduce IP");
+        String host = sc.nextLine();
+
+
 
     }
 }
